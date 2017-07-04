55810427a9e11264b251644fce3b02092a22c333
Yuvaraj D <yuvarajit@hotmail.com>
2017-07-04 03:34:47 -0700

    Initial Project Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float sensitivity=5.0f;
+	public float smoothing = 2.0f;
+	public float lf = 10.0f;
+	Vector2 mouseLook;
+	Vector2 smoothV;
+
+
+	GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		character = this.transform.parent.gameObject;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var md = new Vector2 (Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"));
+		md = Vector2.Scale (md, new Vector2 (sensitivity * smoothing, sensitivity * smoothing));
+		smoothV.x = Mathf.Lerp (smoothV.x, md.x,lf/ smoothing);
+		smoothV.y=Mathf.Lerp (smoothV.y, md.y,lf/ smoothing);
+		mouseLook += smoothV;
+
+		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+		character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x,character.transform.up);
+	}
+}
---
+++ Assets/Scripts/DragController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragController : MonoBehaviour {
+
+
+	public GameObject background;
+	private Renderer rend;
+	public Renderer block;
+	public Texture texture;
+	public Vector3 initialPostion;
+	public Quaternion initialRotation;
+	private bool dragging = false;
+	//private bool change = false;
+	private float distance;
+	void Start() {
+
+		block.GetComponent<Renderer> ();
+		initialPostion = block.transform.position;
+		Debug.Log(initialPostion);
+		initialRotation = block.transform.rotation;
+		Debug.Log(initialRotation);
+
+	}
+
+	void OnMouseEnter()
+	{
+		//Debug.Log("Mouse Clicked");
+	}
+
+	void OnMouseExit()
+	{
+		//Debug.Log("Mouse Moved");
+	}
+
+	void OnMouseDown()
+	{
+		distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		Debug.Log(distance);
+		dragging = true;
+	}
+
+	void OnMouseUp()
+	{
+		//Debug.Log("drop");
+		dragging = false;
+		rend = background.GetComponent<Renderer> ();
+		rend.material.mainTexture = texture;
+		block.transform.position = initialPostion;
+		block.transform.rotation = initialRotation;
+		//change = true;
+	}
+
+	void Update()
+	{
+		if (dragging) {
+			//Debug.Log("drag Started");
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Vector3 rayPoint = ray.GetPoint (distance);
+			transform.position = rayPoint;
+
+		} 
+
+	}
+
+
+}
